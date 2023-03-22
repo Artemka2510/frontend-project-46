c8a78e3e6e964c9c58bcdcece9731ef8c8ac2485
Artem <artem250204@mail.ru>
2023-03-22 10:06:44 +0300

    step 3 files

--- gendiff.js
+++ gendiff.js
@@ -2,7 +2,18 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description(' Compares two configuration files and shows a difference')
-  .version('0.8.0');
+    .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+ 
+
+const genDiff = (file1, file2) => {
+  console.log(file1);
+  console.log(file2);
+};
+
+
 
   program.parse();
