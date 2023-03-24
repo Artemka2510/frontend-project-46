513dcd195c7b4a1f8438e3cf0da4f0410f8e9576
Artem <artem250204@mail.ru>
2023-03-24 15:55:02 +0300

    step 3 update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-
+import genDiff from '../src/genDiff.js'
 const program = new Command();
 
 program
@@ -9,5 +9,7 @@ program
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
   .parse()
-
-
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+  program.parse();
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,30 +1,34 @@
 import path from 'path';
 import fs from 'fs';
-import _, { sortBy } from 'lodash';
+import _ from 'lodash';
 
 const genDiff = (path1, path2) => {
     const file1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path1), 'utf-8'));
     const file2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path2), 'utf-8'));
-
     const keys = _.union(Object.keys(file1), Object.keys(file2));
     const sortedKeys = _.sortBy(keys);
-    
-    const comparison = sortedKeys.flatmap((key) => {
-        if (file1.key !== 'undefined' && file2.key === 'undefined') {
-            
+    const comparison = sortedKeys.flatMap((key) => {
+        if (file1[key] !== undefined && file2[key] === undefined) {
+            return `${'-'} ${key}: ${file1[key]}`
         }
-        if (file1.key === 'undefined' && file2.key !== 'undefined') {
+        if (file1[key] === undefined && file2[key] !== undefined) {            
+            return `${'+'} ${key}: ${file2[key]}`
 
         }
-        if (file1.key !== 'undefined' && file2.key !== 'undefined') {
-            if (file1.key === file2.key) {
+        if (file1[key] !== undefined && file2[key] !== undefined) {   
 
+            if (file1[key] === file2[key]) {
+                return `${key}: ${file1[key]}`;
+                
             }
-        } else {
+        }   if (file1[key] !== file2[key]){
+            return [`${'-'} ${key}: ${file1[key]}`,`${'+'} ${key}: ${file2[key]}` ];
 
-        }
+              
+        } 
 
     });
     return comparison;
 };
+
 export default genDiff;
